b34a2e91fced8d6ccfd6151b9dc47d693ecd37fe
Ana Rodrigues <dev.rodriguesana@gmail.com>
2022-11-01 22:00:51 -0300

    reveal

---
+++ scripts/reveal.js
@@ -0,0 +1,73 @@
+const sr = ScrollReveal({ reset: false })
+
+// ========== ESCADINHA DIREITA PARA ESQUERDA ========
+
+sr.reveal(".right", {
+    origin: "right",
+    distance: "300px",
+    duration: 1500,
+    delay: 50,
+    reset: false
+});
+sr.reveal(".right2", {
+    origin: "right",
+    distance: "500px",
+    duration: 1500,
+    delay: 250,
+    reset: false
+});
+sr.reveal(".right3", {
+    origin: "right",
+    distance: "700px",
+    duration: 1500,
+    delay: 450,
+    reset: false
+});
+sr.reveal(".right4", {
+    origin: "right",
+    distance: "900px",
+    duration: 1500,
+    delay: 650,
+    reset: false
+});
+
+// ========== ESCADINHA ESQUERDA PARA DIREITA ========
+
+sr.reveal(".left", {
+    origin: "left",
+    distance: "300px",
+    duration: 1500,
+    delay: 50,
+    reset: false
+});
+sr.reveal(".left2", {
+    origin: "left",
+    distance: "500px",
+    duration: 1500,
+    delay: 250,
+    reset: false
+});
+sr.reveal(".left3", {
+    origin: "left",
+    distance: "700px",
+    duration: 1500,
+    delay: 450,
+    reset: false
+});
+sr.reveal(".left4", {
+    origin: "left",
+    distance: "900px",
+    duration: 1500,
+    delay: 650,
+    reset: false
+});
+
+// ========== DE CIMA PARA BAIXO ========
+
+sr.reveal(".top", {
+    origin: "top",
+    distance: "400px",
+    duration: 1700,
+    delay: 100,
+    reset: false
+});
